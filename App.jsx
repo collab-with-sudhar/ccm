b4641677b50299d39771503e5db5aa2ba201d1a6
Sudharshanan <88190543+collab-with-sudhar@users.noreply.github.com>
2025-10-10 09:44:53 +0530

    auth0 app.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import DropboxManager from "./components/DropboxManager";
+import {useAuth0} from '@auth0/auth0-react'
+import './App.css'
 
 function App() {
+  const {isAuthenticated,isLoading,logout,user,loginWithRedirect}=useAuth0()
+
+  if(isLoading) return <>Loading</>
+
   return (
-    <div className="App">
-      <h1>Dropbox File Manager</h1>
-      <DropboxManager />
-    </div>
-  );
+    <>
+      {!isAuthenticated?
+            <><h3>welcome</h3> <button onClick={()=>loginWithRedirect()}>login</button></>
+            :
+            <>
+              <h2>welcome {user.name}</h2>
+              <img src={user.picture} alt="" srcset="" />
+              {user.email}
+              <button onClick={()=>logout({logoutParams:{returnTo:window.location.origin}})}>logout</button>
+            </>}
+    </>
+  )
 }
 
-export default App;
+export default App
